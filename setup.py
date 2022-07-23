1e785e62d7024d1102cd23cdd80b8f231e6c35c9
Takuya Okuno <iwashi1629@gmail.com>
2022-07-23 23:46:50 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="dynamodb-single-table",
+    version="0.0.0",
+    install_requires=[
+    ],
+    extras_require={
+        "develop": [
+        ]
+    },
+    entry_points={
+    }
+)
